13568f9be09a46494e94004558209e59cec9f823
Gizem <gizemodun28@gmail.com>
2022-03-18 11:44:25 +0100

    Update firebase.js

--- src/helper/firebase.js
+++ src/helper/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp} from "firebase/app";
+// import { initializeApp} from "firebase/app";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 import { getDatabase} from "firebase/database"
 
