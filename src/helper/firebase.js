f601409c7991e201d2fbc4801fdcbead29b27fb5
gizem28 <gizemodun28@gmail.com>
2022-04-19 10:13:51 +0200

    last

--- src/helper/firebase.js
+++ src/helper/firebase.js
@@ -2,15 +2,17 @@ import { initializeApp} from "firebase/app";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 import { getDatabase} from "firebase/database"
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBPolMo-OvXnDOS7J0peLcGdHmGvGjRmkQ",
-  authDomain: "social-blog2.firebaseapp.com",
-  databaseURL: "https://social-blog2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "social-blog2",
-  storageBucket: "social-blog2.appspot.com",
-  messagingSenderId: "310658077183",
-  appId: "1:310658077183:web:30833f895c5848587497ca"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL:process.env.REACT_APP_DATABASE_URL,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+  };
+
 
 // Initialize Firebase
 export const firebase = initializeApp(firebaseConfig);
